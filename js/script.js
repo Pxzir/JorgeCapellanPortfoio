44aef786fdff07f035d539f0ca931e83230e8fb8
Jorge <jorgecapellan98@gmail.com>
2023-06-23 16:18:32 -0400

    fix navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// Nav bar
+
+window.addEventListener("scroll", function() {
+    var header = document.getElementById("header");
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+
+
+// Responsive 
+
 const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
     document.querySelector(".portfolio-navbar").classList.toggle("show")
